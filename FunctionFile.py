8448a7e5c0e4812d7a744b19a638dba2dcc79277
davmein0 <144638896+davmein0@users.noreply.github.com>
2024-06-11 21:29:24 -0400

    Convert dV units, edit comments

--- FunctionFile.py
+++ FunctionFile.py
@@ -2,18 +2,19 @@ import numpy as np
 
 def ApogeeRaise(ap0):
     """Calculates the dV required to raise orbit to 410,000 km apostasis"""
-    # v = sqrt(mu*(2/r - 1/a))
+    # Vis-viva eq: v = sqrt(mu*(2/r - 1/a))
+
     # Gravitational parameter (km^3/s^2)
     mu = 398600
 
     # Radius of earth (km)
     eRadius = 6378
     
-    # periapsis distance (km)
+    # periapsis altitude (km)
     r = eRadius + 185
 
     # semi-major axis (km)
-    # a0: initial; af: final
+    # a0: initial; af: final semi-major axis
     a0 = (r + ap0 + eRadius) / 2
     af = (r + 410000 + eRadius) / 2
 
@@ -23,5 +24,12 @@ def ApogeeRaise(ap0):
 
     # Change in velocity at periapsis needed to reach moon (km/s)
     dV = vf - v0
+
+    # Change units to m/s
+    dV = 1000 * dV
+
+    # Round change in velocity to 3 decimal places
+    dV = round(dV, 3)
+
     return dV
 
