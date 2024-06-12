1861924507af46ca1f58fa5dce708a485f10a2c3
davmein0 <144638896+davmein0@users.noreply.github.com>
2024-06-11 21:20:53 -0400

    Rough Draft FunctionFile

--- FunctionFile.py
+++ FunctionFile.py
@@ -1,1 +1,24 @@
 import numpy as np
+
+def ApogeeRaise(ap0):
+    """Calculates the dV required to raise orbit to 410,000 km apostasis"""
+    # v = sqrt(mu*(2/r - 1/a))
+    # Gravitational parameter (km^3/s^2)
+    mu = 398600
+
+    # Radius of earth (km)
+    eRadius = 6378
+    
+    # periapsis distance (km)
+    r = eRadius + 185
+
+    # semi-major axis (km)
+    a0 = (r + ap0 + eRadius) / 2
+    af = (r + 410000 + eRadius) / 2
+
+    v0 = np.sqrt(mu * (2/r - 1/a0))
+    vf = np.sqrt(mu * (2/r - 1/af))
+
+    dV = vf - v0
+    return dV
+
